95b94058bb36fc6c4c3a2c0926362bfcbb420934
陈小勇 <xiaoyong.chen@zhangmen.com>
2021-05-14 02:22:45 +0800

    fix: 去掉多余的代码

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 // main index.js
 
-// import { NativeModules } from 'react-native';
-//
-// const { Drag } = NativeModules;
-//
-// export default Drag;
 import Drag from './src/drag';
 
 export default Drag;
